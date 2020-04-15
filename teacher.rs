d23d89903b77951df4bcc475a0d5011249c3d967
Zain <s.zainrashid98@gmail.com>
2020-04-15 16:54:54 +0500

    Initiliaze teachers

---
+++ teacher.rs
@@ -0,0 +1,13 @@
+pub fn maths() {
+    println!("Maths teacher name is Zain");
+    }
+    pub fn chemistry() {
+    println!("Chemistry teacher name is Bilal");
+    }
+    pub fn physics() {
+    println!("Physics teacher name is Wasiq");
+    }
+    pub fn computer() {
+    println!("Computer teacher name is Hassan");
+    }
+    
