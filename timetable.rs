b13acfe12701bdafbbe8e1d8d52f7014d2684490
Zain <s.zainrashid98@gmail.com>
2020-04-15 16:56:18 +0500

    Initiliaze timetable

---
+++ timetable.rs
@@ -0,0 +1,13 @@
+pub fn maths() {
+    println!("Maths period is 1st");
+    }
+    pub fn chemistry() {
+    println!("Chemistry period is 2nd");
+    }
+    pub fn physics() {
+    println!("Physics period is 3rd");
+    }
+    pub fn computer() {
+    println!("Computer period is 4th");
+    }
+    
