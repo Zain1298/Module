1fe051692a307aad747d011fa9040f546f572a6d
Zain <s.zainrashid98@gmail.com>
2020-04-15 16:55:35 +0500

    Initiliaze syllabus

---
+++ syllabus.rs
@@ -0,0 +1,12 @@
+pub fn maths() {
+    println!("Maths syllabus is about Fraction");
+    }
+    pub fn chemistry() {
+    println!("Chemistry syllabus is about Air and Water");
+    }
+    pub fn physics() {
+    println!("Physics syllabus is about Force");
+    }
+    pub fn computer() {
+    println!("Computer syllabus is about PowerPoint");
+    }
