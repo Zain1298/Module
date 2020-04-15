ca3f1db606bf4f4e45bee5dd4a3579e592a61d0e
Zain <s.zainrashid98@gmail.com>
2020-04-15 16:57:38 +0500

    Calling modules

--- main.rs
+++ main.rs
@@ -3,18 +3,18 @@ mod syllabus;
 mod timetable;
 
 fn main() {
-
-
-
-
-
-
-
-
-
-
-
-
-
-
+    teacher::maths();
+    teacher::physics();
+    teacher::chemistry();
+    teacher::computer();
+    println!("\n");
+    syllabus::maths();
+    syllabus::physics();
+    syllabus::chemistry();
+    syllabus::computer();
+    println!("\n");
+    timetable::maths();
+    timetable::computer(); 
+    timetable::physics();
+    timetable::chemistry();
 }
